acad48c6e1fe0a9f2d68461cde658d360afce6c2
celinaj108 <66762723+celinaj108@users.noreply.github.com>
2024-08-27 12:40:16 -0400

    Update core.js

--- core.js
+++ core.js
@@ -20,9 +20,6 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
         updateImage(variantImage) {
             this.image=variantImage
         }
